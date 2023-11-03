3cf545ab3fa2f59a5aabaeb1c508063debebe62d
AnetaPy <aneta.pych15@gmail.com>
2023-11-03 17:23:22 +0100

    Added getOne and deleteMaterial methods to MaterialRecord.

--- records/material.record.ts
+++ records/material.record.ts
@@ -16,12 +16,21 @@ export class MaterialRecord implements ItemEntity {
         this.amount = obj.amount;
     }
 
-//     List all materials from database.
+    // Find one material.
+    static async getOne(id: string): Promise<MaterialRecord | null> {
+        const [results] = await pool.execute("SELECT * FROM `materials` WHERE `id` = :id", {
+            id,
+        }) as any as MaterialRecordResult;
+        return results.length === 0 ? null : new MaterialRecord(results[0]);
+    }
+
+    // List all materials from database.
     static async listAllMaterials(): Promise<ItemEntity[] | null> {
         const [results] = await pool.execute("SELECT * FROM `materials`") as any as MaterialRecordResult;
         return results.map(obj => new MaterialRecord(obj));
     }
 
+    // Insert new material.
     async insertNewMaterial(): Promise<void> {
         if (!this.id) {
             this.id = uuid();
@@ -30,6 +39,13 @@ export class MaterialRecord implements ItemEntity {
         }
 
         await pool.execute("INSERT INTO `materials`(`id`, `name`, `amount`) VALUES (:id, :name, :amount)", this)
+    }
+
+    // Delete material.
+    async deleteMaterial(): Promise<void> {
+        await pool.execute("DELETE FROM `materials` WHERE `id` = :id", {
+            id: this.id,
+        })
     }
 
 }

1767a2a3f29f33d29920e8c7e9c91e7a68b09c48
AnetaPy <aneta.pych15@gmail.com>
2023-11-03 12:50:49 +0100

    Added insertNewMaterial method to MaterialRecord.

--- records/material.record.ts
+++ records/material.record.ts
@@ -1,5 +1,6 @@
 import {FieldPacket} from "mysql2";
 import {pool} from "../utils/db";
+import {v4 as uuid} from 'uuid';
 import {ItemEntity} from "../types";
 
 type MaterialRecordResult = [ItemEntity[], FieldPacket];
@@ -19,6 +20,16 @@ export class MaterialRecord implements ItemEntity {
     static async listAllMaterials(): Promise<ItemEntity[] | null> {
         const [results] = await pool.execute("SELECT * FROM `materials`") as any as MaterialRecordResult;
         return results.map(obj => new MaterialRecord(obj));
+    }
+
+    async insertNewMaterial(): Promise<void> {
+        if (!this.id) {
+            this.id = uuid();
+        } else {
+            throw new Error('Nie można dodać materiału któy już istnieje!')
+        }
+
+        await pool.execute("INSERT INTO `materials`(`id`, `name`, `amount`) VALUES (:id, :name, :amount)", this)
     }
 
 }

f265cf540256c481369e6707084449519a5e613c
AnetaPy <aneta.pych15@gmail.com>
2023-11-08 19:23:24 +0100

    Added RelationElementEntity interface.

--- types/relation/relation-entity.ts
+++ types/relation/relation-entity.ts
@@ -1,6 +1,10 @@
 export interface RelationEntity {
     id: string;
     order_id: string;
-    item_id: string;
+    item_id?: string;
     amount: number;
+}
+
+export interface RelationElementEntity extends RelationEntity{
+    element_id: string;
 }

7341753cc6e5b87641bce098cd1b05e3e2f57c89
AnetaPy <aneta.pych15@gmail.com>
2023-11-08 19:08:13 +0100

    Added find and delete element functions.

--- records/element.record.ts
+++ records/element.record.ts
@@ -1,6 +1,9 @@
 import {pool} from "../utils/db";
 import {v4 as uuid} from 'uuid';
 import {ItemEntity} from "../types";
+import {FieldPacket} from "mysql2";
+
+type ElementRecordResult = [ItemEntity[], FieldPacket];
 
 export class ElementRecord implements ItemEntity {
     id: string;
@@ -13,6 +16,14 @@ export class ElementRecord implements ItemEntity {
         this.amount = obj.amount;
     }
 
+    // Find one element.
+    static async getOneElement(id: string): Promise<ElementRecord | null> {
+        const [results] = await pool.execute("SELECT * FROM `elements` WHERE `id` = :id", {
+            id,
+        }) as any as ElementRecordResult;
+        return results.length === 0 ? null : new ElementRecord(results[0]);
+    }
+
     // Insert new element.
     async insertNewElement(): Promise<void> {
         if (!this.id) {
@@ -22,5 +33,12 @@ export class ElementRecord implements ItemEntity {
         }
 
         await pool.execute("INSERT INTO `elements`(`id`, `name`, `amount`) VALUES (:id, :name, :amount)", this)
+    }
+
+    // Delete element.
+    async deleteElement(): Promise<void> {
+        await pool.execute("DELETE FROM `elements` WHERE `id` = :id", {
+            id: this.id,
+        })
     }
 }
